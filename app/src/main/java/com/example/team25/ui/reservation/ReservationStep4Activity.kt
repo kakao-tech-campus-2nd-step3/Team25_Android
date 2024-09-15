17b69a360b2b86ce1ec777281a7425de55969e34
changukLee <ckddnr5527@gmail.com>
2024-09-16 00:43:58 +0900

    design: 예약 step4 화면 디자인 추가

---
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep4Activity.kt
@@ -0,0 +1,13 @@
+package com.example.team25.ui.reservation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.team25.R
+
+class ReservationStep4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation_step4)
+    }
+}
