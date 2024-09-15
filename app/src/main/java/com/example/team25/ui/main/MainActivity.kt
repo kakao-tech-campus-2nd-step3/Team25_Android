8ac5d6329b991e3e1f17dc9d157b5878eb5060c5
Jeonghoon Park <hoondb@naver.com>
2024-09-16 01:30:34 +0900

    feat: 실시간 동행 현황 화면으로 이동하는 기능 추가

--- app/src/main/java/com/example/team25/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/team25/ui/main/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.team25.ui.main
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.team25.R
+import com.example.team25.ui.main.companion.LiveCompanionActivity
+import com.example.team25.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToLiveCompanion()
+    }
+
+    private fun navigateToLiveCompanion() {
+        binding.realTimeCompanionSeeAllBtn.setOnClickListener {
+            val intent = Intent(this, LiveCompanionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
