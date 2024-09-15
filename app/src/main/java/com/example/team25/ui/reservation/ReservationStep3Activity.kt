adfd222911592c446bd161406eae5cb25221bb5d
changukLee <ckddnr5527@gmail.com>
2024-09-16 00:39:54 +0900

    design: 예약 step3 화면 디자인 추가

---
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep3Activity.kt
@@ -0,0 +1,12 @@
+package com.example.team25.ui.reservation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.team25.R
+
+class ReservationStep3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation_step3)
+    }
+}
