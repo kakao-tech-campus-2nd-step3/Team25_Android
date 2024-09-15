af73da072f84126acf198c5f253a39531ccc7fc6
changukLee <ckddnr5527@gmail.com>
2024-09-16 01:30:33 +0900

    design: 예약 step11 화면 디자인 추가

--- app/src/main/java/com/example/team25/ui/reservation/ReservationStep10Activity.kt
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep10Activity.kt
@@ -3,7 +3,6 @@ package com.example.team25.ui.reservation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.team25.R
 import com.example.team25.databinding.ActivityReservationStep10Binding
 
 class ReservationStep10Activity : AppCompatActivity() {
@@ -16,6 +15,7 @@ class ReservationStep10Activity : AppCompatActivity() {
         binding.name.text = "김지수"
         navigateToMain()
         navigateToPrevious()
+        navigateToNext()
     }
 
     private fun navigateToMain() {
@@ -27,6 +27,13 @@ class ReservationStep10Activity : AppCompatActivity() {
     private fun navigateToPrevious() {
         binding.backBtn.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun navigateToNext() {
+        binding.nextBtn.setOnClickListener {
+            val intent = Intent(this@ReservationStep10Activity, ReservationStep11Activity::class.java)
+            startActivity(intent)
         }
     }
 }
