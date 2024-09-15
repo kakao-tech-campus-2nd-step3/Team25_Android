c2e1f4162f9dc1891b1ccebfd9876189333be28e
changukLee <ckddnr5527@gmail.com>
2024-09-16 01:36:45 +0900

    fix: 예약 step11 화면 이동 버튼 제거

--- app/src/main/java/com/example/team25/ui/reservation/ReservationStep11Activity.kt
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep11Activity.kt
@@ -2,11 +2,23 @@ package com.example.team25.ui.reservation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.team25.R
+import com.example.team25.databinding.ActivityReservationStep11Binding
 
 class ReservationStep11Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityReservationStep11Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation_step11)
+        binding = ActivityReservationStep11Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToPrevious()
+
+    }
+
+    private fun navigateToPrevious() {
+        binding.backBtn.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
