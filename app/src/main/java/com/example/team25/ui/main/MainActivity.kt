edeb43e23a66b37774845c22fcb44d6f24918583
Jeonghoon Park <hoondb@naver.com>
2024-09-16 02:58:43 +0900

    feat: 예약 현황 화면으로 이동하는 기능 추가

--- app/src/main/java/com/example/team25/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/team25/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.team25.ui.main.companion.LiveCompanionActivity
 import com.example.team25.databinding.ActivityMainBinding
+import com.example.team25.ui.main.status.ReservationStatusActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,11 +15,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         navigateToLiveCompanion()
+        navigateToReservationStatus()
     }
 
     private fun navigateToLiveCompanion() {
         binding.realTimeCompanionSeeAllBtn.setOnClickListener {
             val intent = Intent(this, LiveCompanionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun navigateToReservationStatus() {
+        binding.reservationSeeAllBtn.setOnClickListener {
+            val intent = Intent(this, ReservationStatusActivity::class.java)
             startActivity(intent)
         }
     }
