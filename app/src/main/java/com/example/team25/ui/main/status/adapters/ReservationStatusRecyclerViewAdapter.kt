d11cb597bbdf7f3af8573373c3a98966cbcdb5c8
Jeonghoon Park <hoondb@naver.com>
2024-09-16 02:40:30 +0900

    design: 예약 취소 화면 디자인 추가

--- app/src/main/java/com/example/team25/ui/main/status/adapters/ReservationStatusRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/team25/ui/main/status/adapters/ReservationStatusRecyclerViewAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.team25.databinding.ItemReservationStatusBinding
+import com.example.team25.ui.main.status.ReservationCancelActivity
 import com.example.team25.ui.main.status.data.ReservationInfo
 import java.text.SimpleDateFormat
 import java.util.Locale
