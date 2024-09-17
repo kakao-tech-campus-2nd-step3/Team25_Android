0d77a377eed4962a8d26880eef4b835560e6fbcb
Jeonghoon Park <hoondb@naver.com>
2024-09-17 23:09:20 +0900

    refactor: 싱글톤 객체 생성 코드 삭제

--- app/src/main/java/com/example/team25/database/HospitalDatabase.kt
+++ app/src/main/java/com/example/team25/database/HospitalDatabase.kt
@@ -1,30 +1,11 @@
 package com.example.team25.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.team25.dao.HospitalDao
 import com.example.team25.domain.HospitalDomain
 
 @Database(entities = [HospitalDomain::class], version = 1)
-abstract class HospitalDatabase: RoomDatabase() {
+abstract class HospitalDatabase : RoomDatabase() {
     abstract fun hospitalDao(): HospitalDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: HospitalDatabase? = null
-
-        fun getInstance(context: Context): HospitalDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    HospitalDatabase::class.java,
-                    "hospitals"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
