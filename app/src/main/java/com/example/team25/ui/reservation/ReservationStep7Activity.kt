683cd604081376e9b854b872599085602f43a612
Jeonghoon Park <hoondb@naver.com>
2024-09-17 15:56:29 +0900

    feat: retrofit 객체 생성 함수 추가

--- app/src/main/java/com/example/team25/ui/reservation/ReservationStep7Activity.kt
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep7Activity.kt
@@ -8,17 +8,33 @@ import com.example.team25.databinding.ActivityReservationStep7Binding
 import com.example.team25.domain.HospitalDomain
 import com.example.team25.ui.reservation.adapters.HospitalRecyclerViewAdapter
 import com.example.team25.ui.reservation.interfaces.OnHospitalClickListener
+import com.example.team25.ui.reservation.network.KakaoApi
+import com.example.team25.ui.reservation.network.RemoteSearchHospitalService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ReservationStep7Activity : AppCompatActivity() {
     private lateinit var binding: ActivityReservationStep7Binding
+    private lateinit var remoteService: RemoteSearchHospitalService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReservationStep7Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initializeRemoteSearchHospitalService()
         setSearchResultRecyclerView()
         navigateToPrevious()
+    }
+
+    private fun initializeRemoteSearchHospitalService() {
+        val retrofitService = Retrofit.Builder()
+            .baseUrl("https://dapi.kakao.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(KakaoApi::class.java)
+
+        remoteService = RemoteSearchHospitalService(retrofitService)
     }
 
     private fun setSearchResultRecyclerView() {
