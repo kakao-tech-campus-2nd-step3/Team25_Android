3c09cc1c6686291193f5f7f0e1a7c785f99b48c7
Jeonghoon Park <hoondb@naver.com>
2024-09-17 15:45:44 +0900

    feat: 카카오 api 응답 처리를 위한 클래스 생성
    - 키워드 검색 사용
    - 병원 카테고리(HP8) 검색 결과만 출력 가능

---
+++ app/src/main/java/com/example/team25/ui/reservation/network/RemoteSearchHospitalService.kt
@@ -0,0 +1,35 @@
+package com.example.team25.ui.reservation.network
+
+import com.example.team25.BuildConfig
+import com.example.team25.domain.HospitalDomain
+import com.example.team25.ui.reservation.interfaces.SearchHospitalService
+
+
+class RemoteSearchHospitalService(private val kakaoApi: KakaoApi): SearchHospitalService {
+
+    override suspend fun getSearchedResult(keyword: String, page: Int): List<HospitalDomain> {
+        val hospitals = mutableListOf<HospitalDomain>()
+
+        val response = kakaoApi.getSearchKeyword(
+            key = BuildConfig.KAKAO_REST_API_KEY,
+            categoryGroupCode = "HP8",
+            query = keyword,
+            size = 15,
+            page = page
+        )
+
+        if (response.isSuccessful) {
+            response.body()?.documents?.let { documents ->
+                hospitals.addAll(documents.map { document ->
+                    HospitalDomain(
+                        name = document.name,
+                        address = document.address,
+                    )
+                })
+            }
+        } else {
+            throw RuntimeException("API 요청 실패: ${response.errorBody()?.string()}")
+        }
+        return hospitals
+    }
+}
