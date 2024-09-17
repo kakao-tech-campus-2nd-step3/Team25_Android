0e13528f14e29042c7769fade51d25788bbf0e6a
Jeonghoon Park <hoondb@naver.com>
2024-09-17 15:31:25 +0900

    refactor: HospitalDomain 수정
    - parcelize 사용

--- app/src/main/java/com/example/team25/domain/HospitalDomain.kt
+++ app/src/main/java/com/example/team25/domain/HospitalDomain.kt
@@ -1,6 +1,11 @@
 package com.example.team25.domain
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class HospitalDomain(
-    val name: String,
-    val address: String,
-)
+    @SerializedName("place_name") val name: String,
+    @SerializedName("road_address_name") val address: String,
+):Parcelable
