abf8c6c5312287e7fdb5ba825ba2bc6a20852d40
Jeonghoon Park <hoondb@naver.com>
2024-09-17 16:06:55 +0900

    feat: 병원 검색 기능 구현
    - 검색어 정확도 순, 글자 수 순으로 정렬

--- app/src/main/java/com/example/team25/ui/reservation/ReservationStep7Activity.kt
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep7Activity.kt
@@ -2,6 +2,8 @@ package com.example.team25.ui.reservation
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.team25.databinding.ActivityReservationStep7Binding
@@ -10,12 +12,16 @@ import com.example.team25.ui.reservation.adapters.HospitalRecyclerViewAdapter
 import com.example.team25.ui.reservation.interfaces.OnHospitalClickListener
 import com.example.team25.ui.reservation.network.KakaoApi
 import com.example.team25.ui.reservation.network.RemoteSearchHospitalService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ReservationStep7Activity : AppCompatActivity() {
     private lateinit var binding: ActivityReservationStep7Binding
     private lateinit var remoteService: RemoteSearchHospitalService
+    private lateinit var hospitalRecyclerViewAdapter: HospitalRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,7 @@ class ReservationStep7Activity : AppCompatActivity() {
         setContentView(binding.root)
 
         initializeRemoteSearchHospitalService()
+        setHospitalSearchListener()
         setSearchResultRecyclerView()
         navigateToPrevious()
     }
@@ -37,26 +44,64 @@ class ReservationStep7Activity : AppCompatActivity() {
         remoteService = RemoteSearchHospitalService(retrofitService)
     }
 
-    private fun setSearchResultRecyclerView() {
-        val hospitals = mutableListOf<HospitalDomain>()
+    private fun setHospitalSearchListener() {
+        binding.destinationEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-        repeat(20) {
-            hospitals.add(HospitalDomain("병원${it + 1}", "부산광역시 서구 구덕로 ${it + 170}"))
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val keyword = s.toString()
+                if (keyword.isNotBlank()) {
+                    searchHospitals(keyword)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun searchHospitals(keyword: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val hospitals = remoteService.getSearchedResult(keyword, 1)
+                val sortedHospitals = sortHospitals(hospitals, keyword)
+
+                hospitalRecyclerViewAdapter.submitList(sortedHospitals)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
+    }
 
+    /*
+    정렬 기준
+    1. 검색어가 이름에서 더 앞쪽에 나올수록 우선
+    ( 검색어가 이름에 포함되어 있지 않은 경우 가장 낮은 우선순위 )
+
+    ex)
+    검색어: 부산대
+    우선순위: 부산대학교 병원 > 양산부산대학교병원
+
+    2. 글자수가 적은 항목이 우선
+    */
+    private fun sortHospitals(hospitals: List<HospitalDomain>, keyword: String): List<HospitalDomain> {
+        return hospitals.sortedWith(compareBy(
+            { if (it.name.indexOf(keyword) == -1) Int.MAX_VALUE else it.name.indexOf(keyword) },
+            { it.name.length }
+        ))
+    }
+
+    private fun setSearchResultRecyclerView() {
         val hospitalClickListener =
             object : OnHospitalClickListener {
                 override fun onHospitalClicked() {
-                    val intent = Intent(this@ReservationStep7Activity, ReservationStep8Activity::class.java)
+                    val intent = Intent(this@ReservationStep7Activity, ReservationStep7Activity::class.java)
                     startActivity(intent)
                 }
             }
 
-        val adapter = HospitalRecyclerViewAdapter(hospitalClickListener)
-        binding.hospitalRecyclerView.adapter = adapter
+        hospitalRecyclerViewAdapter = HospitalRecyclerViewAdapter(hospitalClickListener)
+        binding.hospitalRecyclerView.adapter = hospitalRecyclerViewAdapter
         binding.hospitalRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        adapter.submitList(hospitals)
     }
 
     private fun navigateToPrevious() {
