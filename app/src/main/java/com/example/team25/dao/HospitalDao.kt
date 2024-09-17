e347608576bfb49fc74db3e0c77c8fec5879e9dd
Jeonghoon Park <hoondb@naver.com>
2024-09-17 20:33:25 +0900

    feat: 병원 정보 저장 room database 생성
    - HospitalDao
    - HospitalDatabase
    - HospitalDomain 수정 ( 엔티티 설정 )

---
+++ app/src/main/java/com/example/team25/dao/HospitalDao.kt
@@ -0,0 +1,15 @@
+package com.example.team25.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.team25.domain.HospitalDomain
+
+@Dao
+interface HospitalDao {
+    @Insert
+    suspend fun insertAll(hospitals: List<HospitalDomain>)
+
+    @Query("SELECT * FROM hospitals WHERE name LIKE '%' || :keyword || '%' LIMIT :pageSize OFFSET :offset")
+    suspend fun searchHospitalsByName(keyword: String, pageSize: Int, offset: Int): List<HospitalDomain>
+}
