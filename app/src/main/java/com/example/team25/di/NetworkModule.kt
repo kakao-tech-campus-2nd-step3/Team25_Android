7e1de42946ce051341f2967013d0890497bae9fd
Jeonghoon Park <hoondb@naver.com>
2024-09-26 00:48:56 +0900

    refactor: 네트워크 모듈 수정

--- app/src/main/java/com/example/team25/di/NetworkModule.kt
+++ app/src/main/java/com/example/team25/di/NetworkModule.kt
@@ -1,6 +1,8 @@
 package com.example.team25.di
 
+import com.example.team25.ui.reservation.interfaces.SearchHospitalService
 import com.example.team25.ui.reservation.network.KakaoApi
+import com.example.team25.ui.reservation.services.RemoteSearchHospitalService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,6 +14,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit {
@@ -28,4 +31,11 @@ object NetworkModule {
     fun provideKakaoApi(retrofit: Retrofit): KakaoApi {
         return retrofit.create(KakaoApi::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideSearchHospitalService(
+        kakaoApi: KakaoApi,
+    ): SearchHospitalService =
+        RemoteSearchHospitalService(kakaoApi)
 }
