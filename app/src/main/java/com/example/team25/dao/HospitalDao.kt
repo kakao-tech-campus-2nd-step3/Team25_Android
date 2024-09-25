44783c9b920eb746a81cccf71b70c01126f2686e
Jeonghoon Park <hoondb@naver.com>
2024-09-26 01:01:46 +0900

    refactor: 병원 정보 엔티티 명 변경

--- app/src/main/java/com/example/team25/dao/HospitalDao.kt
+++ app/src/main/java/com/example/team25/dao/HospitalDao.kt
@@ -3,16 +3,16 @@ package com.example.team25.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import com.example.team25.domain.HospitalDomain
+import com.example.team25.entity.HospitalEntity
 
 @Dao
 interface HospitalDao {
     @Insert
-    suspend fun insertAll(hospitals: List<HospitalDomain>)
+    suspend fun insertAll(hospitals: List<HospitalEntity>)
 
     @Query("SELECT * FROM hospitals LIMIT 1")
-    suspend fun getAnyHospital(): HospitalDomain?
+    suspend fun getAnyHospital(): HospitalEntity?
 
     @Query("SELECT * FROM hospitals WHERE name LIKE '%' || :keyword || '%' LIMIT :pageSize OFFSET :offset")
-    suspend fun searchHospitalsByName(keyword: String, pageSize: Int, offset: Int): List<HospitalDomain>
+    suspend fun searchHospitalsByName(keyword: String, pageSize: Int, offset: Int): List<HospitalEntity>
 }
--- app/src/main/java/com/example/team25/entity/HospitalEntity.kt
+++ app/src/main/java/com/example/team25/entity/HospitalEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "hospitals")
-data class HospitalDomain(
+data class HospitalEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "address") val address: String
