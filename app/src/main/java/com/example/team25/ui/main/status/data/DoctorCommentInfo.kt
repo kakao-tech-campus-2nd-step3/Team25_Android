1f2f7165179825c3718d26a1bd430d0e3f3146de
Jeonghoon Park <hoondb@naver.com>
2024-09-26 23:58:01 +0900

    refactor: json 호출 로직 수정

---
+++ app/src/main/java/com/example/team25/ui/main/status/data/DoctorCommentInfo.kt
@@ -0,0 +1,9 @@
+package com.example.team25.ui.main.status.data
+
+data class DoctorCommentInfo(
+    val doctorOpinion: String,
+    val timeCycle: String?,
+    val mealTime: String?,
+    val time: String?
+)
+
