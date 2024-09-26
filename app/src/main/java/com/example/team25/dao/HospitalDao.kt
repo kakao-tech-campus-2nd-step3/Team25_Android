aeb40799f2b3b30aff4396146c3452039961df9f
Jeonghoon Park <hoondb@naver.com>
2024-09-27 00:56:59 +0900

    fix: dao 충돌 해결

--- app/src/main/java/com/example/team25/dao/HospitalDao.kt
+++ app/src/main/java/com/example/team25/dao/HospitalDao.kt
@@ -12,15 +12,4 @@ interface HospitalDao {
 
     @Query("SELECT * FROM hospitals LIMIT 1")
     suspend fun getAnyHospital(): HospitalEntity?
-
-    @Query("SELECT * FROM hospitals WHERE name LIKE '%' || :keyword || '%' LIMIT :pageSize OFFSET :offset")
-<<<<<<< HEAD
-    suspend fun searchHospitalsByName(
-        keyword: String,
-        pageSize: Int,
-        offset: Int,
-    ): List<HospitalDomain>
-=======
-    suspend fun searchHospitalsByName(keyword: String, pageSize: Int, offset: Int): List<HospitalEntity>
->>>>>>> 1f2f7165179825c3718d26a1bd430d0e3f3146de
 }
