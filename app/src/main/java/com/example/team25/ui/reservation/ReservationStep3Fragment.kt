aa558a22a7b982a7b71a50a7265e7b64e4cf4c89
changuk Lee <ckddnr5527@gmail.com>
2024-09-27 15:52:20 +0900

    feat: 예약 Step4 정보 입력 기능 추가

--- app/src/main/java/com/example/team25/ui/reservation/ReservationStep3Fragment.kt
+++ app/src/main/java/com/example/team25/ui/reservation/ReservationStep3Fragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.team25.R
