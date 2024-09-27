d7f9e3a2754133568f5d52a9e5f934e169dd2361
Jeonghoon Park <hoondb@naver.com>
2024-09-27 17:28:12 +0900

    refactor: 병원 정보 장소 id 필드 추가

--- app/src/main/java/com/example/team25/ui/reservation/network/dto/HospitalDto.kt
+++ app/src/main/java/com/example/team25/ui/reservation/network/dto/HospitalDto.kt
@@ -3,6 +3,7 @@ package com.example.team25.ui.reservation.network.dto
 import com.google.gson.annotations.SerializedName
 
 data class HospitalDto(
+    @SerializedName("id") val id: String,
     @SerializedName("place_name") val name: String,
     @SerializedName("road_address_name") val address: String
 )
