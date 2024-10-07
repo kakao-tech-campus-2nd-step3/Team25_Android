377a7d0e6f2c8a4341a9476ca1726acac7bf4485
changuk Lee <ckddnr5527@gmail.com>
2024-10-07 21:58:24 +0900

    refactor: Utils 인덴트 수정 및 init 제거

--- app/src/main/java/com/example/team25/data/Utils.kt
+++ app/src/main/java/com/example/team25/data/Utils.kt
@@ -7,50 +7,37 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class Utils
-    @Inject
-    constructor(
-        @ApplicationContext context: Context,
-    ) {
-        companion object {
-            private const val PREFS = "prefs"
-            private const val ACCESS_TOKEN = "Access_Token"
-            private const val REFRESH_TOKEN = "Refresh_Token"
-            private var prefs: SharedPreferences? = null
-            private var prefsEditor: SharedPreferences.Editor? = null
-            private var instance: Utils? = null
+class Utils @Inject constructor(@ApplicationContext context: Context) {
+    companion object {
+        private const val PREFS = "prefs"
+        private const val ACCESS_TOKEN = "Access_Token"
+        private const val REFRESH_TOKEN = "Refresh_Token"
+        private var prefs: SharedPreferences? = null
+        private var prefsEditor: SharedPreferences.Editor? = null
 
-            @Synchronized
-            fun init(context: Context): Utils {
-                if (instance == null) {
-                    instance = Utils(context)
-                }
-                return instance!!
-            }
-
-            fun setAccessToken(value: String) {
-                prefsEditor?.putString(ACCESS_TOKEN, value)?.apply()
-            }
-
-            fun getAccessToken(defValue: String): String? {
-                return prefs?.getString(ACCESS_TOKEN, defValue)
-            }
-
-            fun setRefreshToken(value: String) {
-                prefsEditor?.putString(REFRESH_TOKEN, value)?.apply()
-            }
-
-            fun getRefreshToken(defValue: String): String? {
-                return prefs?.getString(REFRESH_TOKEN, defValue)
-            }
-
-            fun clearToken() {
-                prefsEditor?.clear()?.apply()
-            }
+        fun setAccessToken(value: String) {
+            prefsEditor?.putString(ACCESS_TOKEN, value)?.apply()
         }
 
-        init {
-            prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
-            prefsEditor = prefs?.edit()
+        fun getAccessToken(defValue: String): String? {
+            return prefs?.getString(ACCESS_TOKEN, defValue)
+        }
+
+        fun setRefreshToken(value: String) {
+            prefsEditor?.putString(REFRESH_TOKEN, value)?.apply()
+        }
+
+        fun getRefreshToken(defValue: String): String? {
+            return prefs?.getString(REFRESH_TOKEN, defValue)
+        }
+
+        fun clearToken() {
+            prefsEditor?.clear()?.apply()
         }
     }
+
+    init {
+        prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+        prefsEditor = prefs?.edit()
+    }
+}
