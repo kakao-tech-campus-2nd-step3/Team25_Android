6da4b84b4e8acef16a0dc1e9866290f83071e051
Jeonghoon Park <hoondb@naver.com>
2024-10-10 05:02:21 +0900

    refactor: 네트워크 모듈 수정

--- app/src/main/java/com/example/team25/di/NetworkModule.kt
+++ app/src/main/java/com/example/team25/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.example.team25.di
 
 import com.example.team25.BuildConfig
 import com.example.team25.data.network.KakaoApi
+import com.example.team25.data.network.interceptor.TokenInterceptor
 import com.example.team25.data.network.services.RemoteSearchHospitalService
 import com.example.team25.data.remote.ManagerApiService
 import com.example.team25.data.remote.SignIn
@@ -10,6 +11,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Authenticator
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -22,7 +25,6 @@ object NetworkModule {
     @KakaoRetrofit
     fun provideKakaoRetrofit(): Retrofit {
         val url = BuildConfig.KAKAO_BASE_URL
-
         return Retrofit.Builder()
             .baseUrl(url)
             .addConverterFactory(GsonConverterFactory.create())
@@ -48,15 +50,35 @@ object NetworkModule {
 
     @Provides
     @Singleton
+    @ServerRetrofit
+    fun provideServerRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val url = BuildConfig.API_BASE_URL
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
     fun provideSearchHospitalService(kakaoApi: KakaoApi): SearchHospitalService = RemoteSearchHospitalService(kakaoApi)
 
     @Provides
     @Singleton
-    fun provideManagerApiService(@GeneralRetrofit retrofit: Retrofit): ManagerApiService =
+    fun provideManagerApiService(@ServerRetrofit retrofit: Retrofit): ManagerApiService =
         retrofit.create(ManagerApiService::class.java)
 
     @Provides
     fun provideSignIn(@GeneralRetrofit retrofit: Retrofit): SignIn {
         return retrofit.create(SignIn::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(tokenInterceptor: TokenInterceptor, httpAuthenticator: Authenticator): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(tokenInterceptor)
+            .authenticator(httpAuthenticator)
+            .build()
 }
