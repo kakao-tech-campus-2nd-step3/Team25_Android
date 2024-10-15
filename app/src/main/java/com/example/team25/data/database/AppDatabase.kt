6dcd3e7df1747ecee8ad899191c79091cfad1117
Jeonghoon Park <hoondb@naver.com>
2024-10-15 17:51:26 +0900

    refactor: 데이터베이스 병원 정보 엔티티 삭제

--- app/src/main/java/com/example/team25/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/team25/data/database/AppDatabase.kt
@@ -2,19 +2,16 @@ package com.example.team25.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.team25.data.dao.HospitalDao
 import com.example.team25.data.dao.ManagerDao
 import com.example.team25.data.dao.ReservationDao
-import com.example.team25.data.entity.HospitalEntity
 import com.example.team25.data.entity.ManagerEntity
 import com.example.team25.data.entity.ReservationEntity
 
 @Database(
-    entities = [HospitalEntity::class, ManagerEntity::class, ReservationEntity::class],
+    entities = [ManagerEntity::class, ReservationEntity::class],
     version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun hospitalDao(): HospitalDao
     abstract fun managerDao(): ManagerDao
     abstract fun reservationDao(): ReservationDao
 }
