ae2ffb8334b24d327a275f7ddfd350216a71aa8e
Jeonghoon Park <hoondb@naver.com>
2024-10-25 18:06:53 +0900

    refactor: 매니저 저장소에 플로우 멤버 추가

--- app/src/main/java/com/example/team25/data/repository/DefaultManagerRepository.kt
+++ app/src/main/java/com/example/team25/data/repository/DefaultManagerRepository.kt
@@ -8,26 +8,26 @@ import com.example.team25.data.network.calladapter.Result.*
 import com.example.team25.data.remote.ManagerApiService
 import com.example.team25.domain.model.ManagerDomain
 import com.example.team25.domain.repository.ManagerRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class DefaultManagerRepository @Inject constructor(
     private val managerDao: ManagerDao,
     private val managerApiService: ManagerApiService
 ) : ManagerRepository {
+    override val managersFlow: Flow<List<ManagerDomain>> = getAllManagersFlow()
+
+    private fun getAllManagersFlow(): Flow<List<ManagerDomain>> {
+        return managerDao.getAllManagers().map { it.asDomainFromEntity() }
+    }
+
     override suspend fun insertManagers(managers: List<ManagerDomain>) {
         return managerDao.insertManagers(managers.asEntity())
     }
 
-    override suspend fun getAllManagers(): List<ManagerDomain> {
-        return managerDao.getAllManagers().asDomainFromEntity()
-    }
-
     override suspend fun getManagersByName(name: String): List<ManagerDomain> {
         return managerDao.getManagersByName(name).asDomainFromEntity()
-    }
-
-    override suspend fun clearManagers() {
-        return managerDao.clearManagers()
     }
 
     override suspend fun fetchManagers() {
--- app/src/main/java/com/example/team25/ui/reservation/ManagerDataViewModel.kt
+++ app/src/main/java/com/example/team25/ui/reservation/ManagerDataViewModel.kt
@@ -3,28 +3,39 @@ package com.example.team25.ui.reservation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.team25.domain.model.ManagerDomain
+import com.example.team25.domain.model.ReservationInfo
 import com.example.team25.domain.repository.ManagerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class ManagerDataViewModel @Inject constructor(private val repository: ManagerRepository) : ViewModel() {
+class ManagerDataViewModel
+@Inject constructor(private val repository: ManagerRepository) : ViewModel() {
     private val _managers = MutableStateFlow<List<ManagerDomain>>(emptyList())
     val managers: StateFlow<List<ManagerDomain>> = _managers
 
     init {
         fetchManagers()
+        updateManagers()
     }
 
     private fun fetchManagers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.fetchManagers()
+        }
+    }
+
+    private fun updateManagers() {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) { repository.fetchManagers() }
-            updateManagers(getAllManagers())
+            repository.managersFlow.collectLatest { collectedManagers ->
+                _managers.value = collectedManagers
+            }
         }
     }
 
@@ -36,10 +47,6 @@ class ManagerDataViewModel @Inject constructor(private val repository: ManagerRe
 
     private suspend fun getManagersByName(name: String): List<ManagerDomain> {
         return repository.getManagersByName(name)
-    }
-
-    private suspend fun getAllManagers(): List<ManagerDomain> {
-        return repository.getAllManagers()
     }
 
     fun updateManagersByName(name: String) {
