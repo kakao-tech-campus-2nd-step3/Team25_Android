52e3616c021be645d5ac39c6edb729c62d20e8b9
Jeonghoon Park <hoondb@naver.com>
2024-10-25 18:34:18 +0900

    refactor: 예약정보 저장소에 플로우 멤버 추가

--- app/src/main/java/com/example/team25/domain/repository/ReservationRepository.kt
+++ app/src/main/java/com/example/team25/domain/repository/ReservationRepository.kt
@@ -1,15 +1,15 @@
 package com.example.team25.domain.repository
 
-import com.example.team25.data.entity.ReservationEntity
 import com.example.team25.domain.model.ReservationInfo
 import com.example.team25.domain.ReservationStatus
+import kotlinx.coroutines.flow.Flow
 
 interface ReservationRepository {
+    val reservationsFlow: Flow<List<ReservationInfo>>
+
     suspend fun fetchReservations()
 
     suspend fun insertReservation(reservations :List<ReservationInfo>)
-
-    suspend fun getAllReservations(): List<ReservationInfo>
 
     suspend fun getReservationsByStatus(status: ReservationStatus): List<ReservationInfo>
 }
