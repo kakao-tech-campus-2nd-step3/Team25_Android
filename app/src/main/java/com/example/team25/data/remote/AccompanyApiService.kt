2ab1a2a19c401a1b0a77141ac7984d0fd1d0a536
Jeonghoon Park <hoondb@naver.com>
2024-11-01 19:39:52 +0900

    feat: 실시간 동행정보 인터페이스 추가

---
+++ app/src/main/java/com/example/team25/data/remote/AccompanyApiService.kt
@@ -0,0 +1,14 @@
+package com.example.team25.data.remote
+
+import com.example.team25.data.network.calladapter.Result
+import com.example.team25.data.network.dto.AccompanyDto
+import com.example.team25.data.network.dto.ServiceResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AccompanyApiService {
+    @GET("/api/tracking/{reservation_id}")
+    suspend fun getAccompanyInfo(
+        @Path("reservation_id") reservationId: String
+    ): Result<ServiceResponse<AccompanyDto>>
+}
