a0a565ab47a1922b304b5712288fbd5a5d83ecdf
Jeonghoon Park <hoondb@naver.com>
2024-11-01 14:12:10 +0900

    style: mapper 클래스 오타 수정

--- app/src/main/java/com/example/team25/data/entity/mapper/ReportMapper.kt
+++ app/src/main/java/com/example/team25/data/entity/mapper/ReportMapper.kt
@@ -1,26 +1,24 @@
 package com.example.team25.data.entity.mapper
 
 import com.example.team25.data.network.dto.ReportDto
-import com.example.team25.domain.MedicinTime
+import com.example.team25.domain.MedicineTime
 import com.example.team25.domain.model.Report
 
 object ReportMapper {
-    fun asDomainFromDto(dto: List<ReportDto>): List<Report> {
-        return dto.map { reportDto ->
-            Report(
+    fun asDomainFromDto(reportDto: ReportDto): Report {
+        return Report(
                 doctorSummary = reportDto.doctorSummary,
                 frequency = reportDto.frequency,
-                medicinTime = when (reportDto.medicinTime) {
-                    "BEFORE_MEAL" -> MedicinTime.BEFORE_MEAL
-                    "AFTER_MEAL" -> MedicinTime.AFTER_MEAL
-                    else -> MedicinTime.UNKNOWN
+                medicineTime = when (reportDto.medicinTime) {
+                    "BEFORE_MEAL" -> MedicineTime.BEFORE_MEAL
+                    "AFTER_MEAL" -> MedicineTime.AFTER_MEAL
+                    else -> MedicineTime.UNKNOWN
                 },
                 timeOfDays = reportDto.timeOfDays
             )
-        }
     }
 }
 
-fun List<ReportDto>?.asDomainFromDto(): List<Report> {
-    return ReportMapper.asDomainFromDto(this.orEmpty())
+fun ReportDto.asDomainFromDto(): Report {
+    return ReportMapper.asDomainFromDto(this)
 }
