bcbcb299fe92e442e80fe2af42eddb71f362aa79
Jeonghoon Park <hoondb@naver.com>
2024-11-01 15:04:35 +0900

    refactor: 리포트 확인 뷰모델 수정

--- app/src/main/java/com/example/team25/ui/main/status/ReservationCheckReportActivity.kt
+++ app/src/main/java/com/example/team25/ui/main/status/ReservationCheckReportActivity.kt
@@ -1,13 +1,15 @@
 package com.example.team25.ui.main.status
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.team25.R
 import com.example.team25.databinding.ActivityReservationCheckReportBinding
+import com.example.team25.domain.MedicineTime
+import com.example.team25.domain.model.Report
 import com.example.team25.domain.model.ReservationInfo
-import com.example.team25.ui.main.status.data.DoctorCommentInfo
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -24,8 +26,9 @@ class ReservationCheckReportActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         navigateToPrevious()
-        setWriterInfo()
-        collectDoctorComment()
+        updateReservationInfo()
+        collectReservationInfo()
+        collectReportInfo()
     }
 
     private fun navigateToPrevious() {
@@ -34,46 +37,68 @@ class ReservationCheckReportActivity : AppCompatActivity() {
         }
     }
 
-    private fun setWriterInfo() {
+    private fun updateReservationInfo() {
         val reservationInfo: ReservationInfo? = intent.getParcelableExtra("ReservationInfo")
-        reservationInfo?.let {
-            val dateFormat = SimpleDateFormat("yy.MM.dd", Locale.KOREAN)
-
-            binding.managerNameTextView.text = it.managerName
-            binding.dateTextView.text = dateFormat.format(it.reservationDate)
+        reservationInfo?.let { reservation ->
+            viewModel.updateReservationInfo(reservation)
         }
     }
 
-    private fun collectDoctorComment() {
+    private fun collectReservationInfo() {
         lifecycleScope.launch {
-            viewModel.doctorCommentInfo.collectLatest { data ->
-                data?.let {
-                    updateReportUI(it)
-                }
+            viewModel.reservationInfo.collectLatest { reservation ->
+                val dateFormat = SimpleDateFormat("yy.MM.dd", Locale.KOREAN)
+
+                binding.managerNameTextView.text = reservation.managerName
+                binding.dateTextView.text = dateFormat.format(reservation.reservationDate)
             }
         }
     }
 
-    private fun updateReportUI(info: DoctorCommentInfo) {
-        binding.doctorOpinionTextView.text = info.doctorOpinion
+    private fun collectReportInfo() {
+        lifecycleScope.launch {
+            viewModel.reportInfo.collectLatest { reportInfo ->
+                updateReportUI(reportInfo)
+            }
+        }
+    }
 
-        info.timeCycle?.let {
-            binding.timeCycleBtn.text = it
+    @SuppressLint("SetTextI18n")
+    private fun updateReportUI(reportInfo: Report) {
+        reportInfo.doctorSummary.let { doctorOpinion ->
+            binding.doctorOpinionTextView.text = doctorOpinion
+        }
+
+        reportInfo.frequency.let { frequency ->
+            binding.timeCycleBtn.text = "매일 ${frequency}회"
             binding.timeCycleBtn.setBackgroundResource(R.drawable.purple_btn_box)
         }
 
-        info.mealTime?.let {
-            when (it) {
-                "식후" -> binding.mealAfterBtn.setBackgroundResource(R.drawable.purple_btn_box)
-                "식전" -> binding.mealBeforeBtn.setBackgroundResource(R.drawable.purple_btn_box)
+        reportInfo.medicineTime.let { medicineTime ->
+            when (medicineTime) {
+                MedicineTime.AFTER_MEAL -> binding.mealAfterBtn.setBackgroundResource(R.drawable.purple_btn_box)
+                MedicineTime.BEFORE_MEAL -> binding.mealBeforeBtn.setBackgroundResource(R.drawable.purple_btn_box)
+                MedicineTime.UNKNOWN -> {}
             }
         }
 
-        info.time?.let {
-            when (it) {
+        reportInfo.timeOfDays.let { timeOfDays ->
+            val times = timeOfDays.split(" ")
+            times.forEach { time ->
+                when (time) {
+                    "아침" -> binding.morningBtn.setBackgroundResource(R.drawable.purple_btn_box)
+                    "점심" -> binding.lunchBtn.setBackgroundResource(R.drawable.purple_btn_box)
+                    "저녁" -> binding.dinnerBtn.setBackgroundResource(R.drawable.purple_btn_box)
+                }
+            }
+        }
+
+        /* api 멤버 추가 예정
+        reportInfo.let { 시간 ->
+            when (시간) {
                 "30분" -> binding.time30minBtn.setBackgroundResource(R.drawable.purple_btn_box)
                 "1시간" -> binding.time1hourBtn.setBackgroundResource(R.drawable.purple_btn_box)
             }
-        }
+        }*/
     }
 }
