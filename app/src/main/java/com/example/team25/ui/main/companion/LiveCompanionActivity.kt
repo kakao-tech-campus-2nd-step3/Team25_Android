5d2b388e5701b64571ed6f3c228d9ae6aab00a6a
Jeonghoon Park <hoondb@naver.com>
2024-11-03 23:48:15 +0900

    refactor: 실시간 동행현황 화면 및 뷰모델 수정

--- app/src/main/java/com/example/team25/ui/main/companion/LiveCompanionActivity.kt
+++ app/src/main/java/com/example/team25/ui/main/companion/LiveCompanionActivity.kt
@@ -2,23 +2,35 @@ package com.example.team25.ui.main.companion
 
 import LiveCompanionRecyclerViewAdapter
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.team25.R
 import com.example.team25.databinding.ActivityLiveCompanionBinding
+import com.example.team25.domain.model.AccompanyInfo
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.camera.CameraUpdateFactory
 import com.kakao.vectormap.label.LabelManager
 import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class LiveCompanionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLiveCompanionBinding
-    private var sampleLatitude = 37.55
-    private var sampleLongitude = 126.98
+    private val kakaoMapDeferred = CompletableDeferred<KakaoMap>()
+    private val liveCompanionViewModel: LiveCompanionViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +40,8 @@ class LiveCompanionActivity : AppCompatActivity() {
 
         startMapView()
         setLiveCompanionRecyclerViewAdapter()
+        collectAccompanyInfo()
+        collectCoordinateInfo()
         navigateToPrevious()
     }
 
@@ -49,19 +63,12 @@ class LiveCompanionActivity : AppCompatActivity() {
     private fun createMapReadyCallback(): KakaoMapReadyCallback {
         return object : KakaoMapReadyCallback() {
             override fun onMapReady(kakaoMap: KakaoMap) {
-                val labelManager = kakaoMap.labelManager
-                if (labelManager != null) {
-                    addLabelsToMap(labelManager)
-                }
-            }
-
-            override fun getPosition(): LatLng {
-                return LatLng.from(sampleLatitude, sampleLongitude)
+                kakaoMapDeferred.complete(kakaoMap)
             }
         }
     }
 
-    private fun addLabelsToMap(labelManager: LabelManager) {
+    private fun updateLabelsToMap(labelManager: LabelManager, latLng: LatLng) {
         val styles =
             LabelStyles.from(
                 LabelStyle.from(R.drawable.marker).setZoomLevel(8),
@@ -69,30 +76,49 @@ class LiveCompanionActivity : AppCompatActivity() {
 
         val labelOptions =
             LabelOptions.from(
-                LatLng.from(sampleLatitude, sampleLongitude),
+                LatLng.from(latLng),
             )
                 .setStyles(styles)
 
+        labelManager.layer?.removeAll()
         labelManager.layer?.addLabel(labelOptions)
+    }
+
+    private fun updateMapLocation(kakaoMap: KakaoMap, latLng: LatLng) {
+        val labelManager = kakaoMap.labelManager
+
+        kakaoMap.moveCamera(CameraUpdateFactory.newCenterPosition(latLng))
+
+        if (labelManager != null) updateLabelsToMap(labelManager, latLng)
+    }
+
+    private fun collectAccompanyInfo() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                liveCompanionViewModel.accompanyInfoList.collectLatest { accompanyInfoList ->
+                    (binding.liveCompanionRecyclerView.adapter as? LiveCompanionRecyclerViewAdapter)
+                        ?.submitList(accompanyInfoList.map { it.statusDescribe })
+                }
+            }
+        }
+    }
+
+    private fun collectCoordinateInfo() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                val kakaoMap = kakaoMapDeferred.await()
+                liveCompanionViewModel.coordinateInfo.collectLatest { coordinates ->
+                    Log.d("pjh","coord")
+                    updateMapLocation(kakaoMap,coordinates)
+                }
+            }
+        }
     }
 
     private fun setLiveCompanionRecyclerViewAdapter() {
         val adapter = LiveCompanionRecyclerViewAdapter()
         binding.liveCompanionRecyclerView.adapter = adapter
         binding.liveCompanionRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        testLiveCompanionRecyclerViewAdapter(adapter)
-    }
-
-    private fun testLiveCompanionRecyclerViewAdapter(adapter: LiveCompanionRecyclerViewAdapter) {
-        val mock =
-            listOf(
-                "24.08.26 21:00 방문 픽업",
-                "24.08.26 21:04 부산대 병원 도착",
-                "24.08.26 21:04 병원 접수 완료",
-            )
-
-        adapter.submitList(mock)
     }
 
     private fun navigateToPrevious() {
