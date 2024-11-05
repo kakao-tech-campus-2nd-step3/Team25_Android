d44623fb06c388a30216e5ee337df364173c7e6f
Jeonghoon Park <hoondb@naver.com>
2024-11-05 18:36:43 +0900

    refactor: reportDto nullable 수정

--- app/src/main/java/com/example/team25/data/network/dto/ReportDto.kt
+++ app/src/main/java/com/example/team25/data/network/dto/ReportDto.kt
@@ -1,10 +1,11 @@
 package com.example.team25.data.network.dto
 
+import com.example.team25.domain.MedicineTime
 import com.google.gson.annotations.SerializedName
 
 data class ReportDto(
-    @SerializedName("doctorSummary") val doctorSummary: String,
-    @SerializedName("frequency") val frequency: String,
-    @SerializedName("medicinTime") val medicinTime: String,
-    @SerializedName("timeOfDays") val timeOfDays: String
+    @SerializedName("doctorSummary") val doctorSummary: String?,
+    @SerializedName("frequency") val frequency: Int?,
+    @SerializedName("medicinTime") val medicinTime: MedicineTime?,
+    @SerializedName("timeOfDays") val timeOfDays: String?
 )
