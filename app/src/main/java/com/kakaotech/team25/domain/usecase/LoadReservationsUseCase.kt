c492d2d77929bf1052e266816ff875725be65c1d
Jeonghoon Park <hoondb@naver.com>
2024-11-08 19:02:40 +0900

    refactor: 예약 정보 갱신 유스케이스 수정

--- app/src/main/java/com/kakaotech/team25/domain/usecase/LoadReservationsUseCase.kt
+++ app/src/main/java/com/kakaotech/team25/domain/usecase/LoadReservationsUseCase.kt
@@ -1,6 +1,5 @@
 package com.kakaotech.team25.domain.usecase
 
-import com.kakaotech.team25.domain.model.ManagerDomain
 import com.kakaotech.team25.domain.model.ReservationInfo
 import com.kakaotech.team25.domain.repository.ManagerRepository
 import com.kakaotech.team25.domain.repository.ReservationRepository
@@ -13,20 +12,12 @@ class LoadReservationsUseCase
     private val managerRepository: ManagerRepository
 ) {
     suspend operator fun invoke(): List<ReservationInfo> {
-        val reservations = reservationRepository.reservationsFlow.first()
-        val managers = managerRepository.managersFlow.first()
-        val managerMapById = managers.associateBy { it.managerId }
-
-        return mapManagerNameToReservations(reservations, managerMapById)
-    }
-
-    private fun mapManagerNameToReservations(
-        reservations: List<ReservationInfo>,
-        managerMapById: Map<String, ManagerDomain>
-    ): List<ReservationInfo> {
-        return reservations.map { reservationInfo ->
-            val managerName = managerMapById[reservationInfo.managerId]?.name ?: ""
-            reservationInfo.copy(managerName = managerName)
+        val reservations = reservationRepository.getReservationsFlow().first()
+        val updatedReservations = reservations.map { reservation ->
+            val managerName = managerRepository.getManagerNameFlow(reservation.managerId).first()
+            reservation.copy(managerName = managerName)
         }
+
+        return updatedReservations
     }
 }
