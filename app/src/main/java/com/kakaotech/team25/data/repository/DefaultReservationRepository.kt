a31edc6e19edc97d85ee94963f4926db413be2b4
Jeonghoon Park <hoondb@naver.com>
2024-11-08 18:59:03 +0900

    refactor: 예약 현황 관련 저장소 수정

--- app/src/main/java/com/kakaotech/team25/data/repository/DefaultReservationRepository.kt
+++ app/src/main/java/com/kakaotech/team25/data/repository/DefaultReservationRepository.kt
@@ -1,39 +1,21 @@
 package com.kakaotech.team25.data.repository
 
-import com.kakaotech.team25.data.dao.ReservationDao
-import com.kakaotech.team25.data.entity.mapper.asDomainFromDto
-import com.kakaotech.team25.data.entity.mapper.asDomainFromEntity
-import com.kakaotech.team25.data.entity.mapper.asEntity
 import com.kakaotech.team25.data.network.calladapter.Result.*
+import com.kakaotech.team25.data.network.dto.mapper.asDomain
 import com.kakaotech.team25.data.remote.ReservationApiService
 import com.kakaotech.team25.domain.model.ReservationInfo
-import com.kakaotech.team25.domain.ReservationStatus
 import com.kakaotech.team25.domain.repository.ReservationRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class DefaultReservationRepository @Inject constructor(
-    private val reservationDao: ReservationDao,
     private val reservationApiService: ReservationApiService
 ) : ReservationRepository {
-    override val reservationsFlow: Flow<List<ReservationInfo>> = getAllReservationsFlow()
-    private fun getAllReservationsFlow(): Flow<List<ReservationInfo>> {
-        return reservationDao.getAllReservationsFlow().map { it.asDomainFromEntity()}
-    }
-
-    override suspend fun getReservationsByStatus(status: ReservationStatus): List<ReservationInfo> {
-        return reservationDao.getReservationsByStatus(status).asDomainFromEntity()
-    }
-
-    override suspend fun insertReservation(reservations: List<ReservationInfo>) {
-        return reservationDao.insertReservations(reservations.asEntity())
-    }
-
-    override suspend fun fetchReservations() {
-        val result = reservationApiService.fetchReservations()
-        if (result is Success) result.body?.data?.let { reservationDtos ->
-            insertReservation(reservationDtos.asDomainFromDto())
+    override fun getReservationsFlow(): Flow<List<ReservationInfo>> = flow{
+        val result = reservationApiService.getReservations()
+        if (result is Success) result.body?.data?.let { reservationDto ->
+            emit(reservationDto.asDomain())
         }
     }
 }
