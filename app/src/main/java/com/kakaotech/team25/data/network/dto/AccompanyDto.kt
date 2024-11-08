1b6fe7270d8d3c0cc26257b28cd422f0b204af40
Jeonghoon Park <hoondb@naver.com>
2024-11-08 18:56:02 +0900

    refactor: api 변경에 따른 dto 수정

--- app/src/main/java/com/kakaotech/team25/data/network/dto/AccompanyDto.kt
+++ app/src/main/java/com/kakaotech/team25/data/network/dto/AccompanyDto.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class AccompanyDto (
     @SerializedName("status") val status: String,
+    @SerializedName("latitude") val latitude: Double = 0.0,
+    @SerializedName("longitude") val longitude: Double = 0.0,
     @SerializedName("statusDate") val statusDate: String,
     @SerializedName("statusDescribe") val statusDescribe: String,
 )
