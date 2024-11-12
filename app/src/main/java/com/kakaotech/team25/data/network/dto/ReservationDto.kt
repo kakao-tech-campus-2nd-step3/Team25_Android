3bf6b846250bf1db8e71d4723a9bb60b457e6541
Jeonghoon Park <hoondb@naver.com>
2024-11-13 06:03:48 +0900

    fix: 예약 정보 mapper 수정

--- app/src/main/java/com/kakaotech/team25/data/network/dto/ReservationDto.kt
+++ app/src/main/java/com/kakaotech/team25/data/network/dto/ReservationDto.kt
@@ -2,6 +2,8 @@ package com.kakaotech.team25.data.network.dto
 
 import com.kakaotech.team25.domain.ReservationStatus
 import com.google.gson.annotations.SerializedName
+import com.kakaotech.team25.data.network.response.ReserveResponse
+import com.kakaotech.team25.domain.model.Patient
 import java.time.LocalDateTime
 
 data class ReservationDto(
@@ -13,5 +15,6 @@ data class ReservationDto(
     @SerializedName("reservationDateTime") val reservationDate: LocalDateTime?,
     @SerializedName("serviceType") val serviceType: String,
     @SerializedName("transportation") val transportation: String,
-    @SerializedName("price") val price: Int
+    @SerializedName("price") val price: Int,
+    @SerializedName("patient") val patient: ReserveResponse.Patient
 )
